c26b8b0fcce58d1b452c41f0947358fd4661b288
yehonadav <yonadav.barilan@gmail.com>
2020-09-10 09:14:55 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,48 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, useRouteMatch, useLocation } from 'react-router-dom';
 
-// onmessage = (evt:MessageEvent) => {
-//   // const port = evt.ports[0];
-//   const port = evt.source;
-//   const data = {...evt.data};
-//   console.log(evt);
-//   // fetch(data).then(res => {
-//   //   // the response is not clonable
-//   //   // so we make a new plain object
-//   //   const obj = {
-//   //     bodyUsed: false,
-//   //     headers: [...res.headers],
-//   //     ok: res.ok,
-//   //     redirected: res.redurected,
-//   //     status: res.status,
-//   //     statusText: res.statusText,
-//   //     type: res.type,
-//   //     url: res.url
-//   //   };
-//   //   console.log(port);
-//   //   port.postMessage(obj);
-//   //
-//   //   // Pipe the request to the port (MessageChannel)
-//   //   const reader = res.body.getReader();
-//   //   const pump = () => reader.read()
-//   //     .then(({value, done}) => done
-//   //       ? port.postMessage(done)
-//   //       : (port.postMessage(value), pump())
-//   //     );
-//   //
-//   //   // start the pipe
-//   //   pump()
-//   // })
-// };
-
-// // Called sometime after postMessage is called
-function receiveMessage(event)
+// Called sometime after postMessage is called
+function receiveMessage(event:MessageEvent)
 {
   console.log(event);
 //
 //   // Do we trust the sender of this message?
-//   // if (event.origin !== "*")
-//   //   return;
+//   if (event.origin !== "*")
+//     return;
 //
 //   // event.source is window.opener
 //   // event.data is "hello there!"
@@ -52,9 +18,7 @@ function receiveMessage(event)
 //   // you must do in any case), a convenient idiom for replying to a
 //   // message is to call postMessage on event.source and provide
 //   // event.origin as the targetOrigin.
-  event.source.postMessage("hi there yourself!  the secret response " +
-    "is: rheeeeet!",
-    event.origin);
+  event.source.postMessage("hi there yourself!  the secret response " + "is: rheeeeet!", event.origin);
 }
 
 window.addEventListener("message", receiveMessage, false);
